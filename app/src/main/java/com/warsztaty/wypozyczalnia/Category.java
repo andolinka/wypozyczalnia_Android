2e0c0f7b98766d36c1181ccc055477fe6fd6f3f3
Michał Tuśnio <m.tusnio@gmail.com>
2015-06-16 20:35:54 +0200

    Pobieranie kategorie

--- app/src/main/java/com/warsztaty/wypozyczalnia/Category.java
+++ app/src/main/java/com/warsztaty/wypozyczalnia/Category.java
@@ -5,10 +5,18 @@ import android.support.v7.app.ActionBarActivity;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 /**
@@ -23,11 +31,17 @@ public class Category extends ActionBarActivity {
 
         ApiController controller = new ApiController(this);
 
-
+        final Category thisActivity = this;
         controller.SendRequest(Request.Method.GET, R.string.api_car_categories, null, new Response.Listener<String>() {
             @Override
             public void onResponse(String res) {
-                Log.d("Category", res);
+                Log.d("Category", "Response: " + res);
+                try {
+                    thisActivity.BuildCategoriesList(new JSONObject(res));
+                }
+                catch(JSONException e) {
+                    Log.d("Category", "Could not parse JSON: " + e.getMessage());
+                }
             }
         }, new ApiController.GenericErrorListener("Category"));
 
@@ -36,7 +50,7 @@ public class Category extends ActionBarActivity {
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.menu_category, menu);
-        final ListView categoryList = (ListView)findViewById(R.id.categoryList);
+
 
         return true;
     }
@@ -55,6 +69,24 @@ public class Category extends ActionBarActivity {
 
         return super.onOptionsItemSelected(item);
     }
-    
+
+
+    public void BuildCategoriesList(JSONObject categories) {
+        ListView categoryList = (ListView)findViewById(R.id.categoryList);
+        List<String> categoriesStrings = new ArrayList<String>();
+        try {
+            JSONArray results = categories.getJSONArray("results");
+
+            for(int i = 0; i < results.length(); i++) {
+                JSONObject obj = results.getJSONObject(i);
+
+                categoriesStrings.add(obj.getString("name"));
+            }
+        }
+        catch(JSONException e) {
+            Log.d("Category", "Could not parse categories due to " + e.getMessage());
+        }
+        categoryList.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, categoriesStrings));
+    }
 
 }
