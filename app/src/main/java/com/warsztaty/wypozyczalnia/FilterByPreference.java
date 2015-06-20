cbd24236f13eaffd1efc1d2aae36239e4afaff42
Michał Tuśnio <m.tusnio@gmail.com>
2015-06-20 05:11:09 +0200

    Dalsza czesc usuwania ustawienia

--- app/src/main/java/com/warsztaty/wypozyczalnia/FilterByPreference.java
+++ app/src/main/java/com/warsztaty/wypozyczalnia/FilterByPreference.java
@@ -48,10 +48,7 @@ public class FilterByPreference extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
+
 
         return super.onOptionsItemSelected(item);
     }
--- app/src/main/java/com/warsztaty/wypozyczalnia/MainPage.java
+++ app/src/main/java/com/warsztaty/wypozyczalnia/MainPage.java
@@ -31,10 +31,6 @@ public class MainPage extends ActionBarActivity {
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
 
         return super.onOptionsItemSelected(item);
     }
