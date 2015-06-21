0f7723bc25107986231f1f60c1c65fcf03256463
Michał Tuśnio <m.tusnio@gmail.com>
2015-06-21 17:05:21 +0200

    Refaktoryzacja + filtry na glownej

--- app/src/main/java/com/warsztaty/wypozyczalnia/FirstPage.java
+++ app/src/main/java/com/warsztaty/wypozyczalnia/FirstPage.java
@@ -20,7 +20,9 @@ import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 /**
  * Created by ewelina on 11.06.15.
@@ -50,22 +52,7 @@ public class FirstPage extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_first_page);
 
-        ApiController controller = new AuthController(this);
-        final FirstPage thisPage = this;
-
-        controller.SendRequest(Request.Method.GET, R.string.api_cars, null, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String res) {
-                Log.d("FirstPage", "Response: " + res);
-                try {
-
-                    thisPage.buildSimpleCarList(new JSONObject(res));
-                }
-                catch(JSONException e) {
-                    Log.d("FirstPage", "Could not parse JSON: " + e.getMessage());
-                }
-            }
-        }, new ApiController.GenericErrorListener("Category"));
+        buildSimpleCarList(null);
 
 //        ListView lista = (ListView)findViewById(R.id.listView);
 //        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -79,35 +66,62 @@ public class FirstPage extends ActionBarActivity {
 //        });
 
     }
-    public void buildSimpleCarList (JSONObject carsDescription){
-        ListView lista = (ListView)findViewById(R.id.listView);
-        List<CarAdapter.CarData> cars = new ArrayList<CarAdapter.CarData>();
-        try {
-            JSONArray results = carsDescription.getJSONArray("results");
 
-            for(int i = 0; i < results.length(); i++) {
-                JSONObject obj = results.getJSONObject(i);
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
 
-                CarAdapter.CarData data = new CarAdapter.CarData(obj);
-                cars.add(data);
+        if (requestCode == FilterByPreference.FILTER_RESULTS) {
+            if(resultCode == RESULT_OK){
+                Map<String, String> filters = (HashMap<String,String>)data.getExtras().getSerializable("filters");
+
+                buildSimpleCarList(filters);
             }
         }
-        catch(JSONException e) {
-            Log.d("FirstPage", "Could not parse description due to " + e.getMessage());
-        }
-        ArrayAdapter adapter = new CarAdapter(this, R.layout.elementy_listy_main, cars);
+    }
 
-        lista.setAdapter(adapter);
-
-        lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+    public void buildSimpleCarList (Map<String, String> filters){
+        ApiController controller = new AuthController(this);
+        controller.SendRequest(Request.Method.GET, R.string.api_cars, filters, new Response.Listener<String>() {
             @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Intent intent = new Intent(FirstPage.this, RentACar.class);
-                CarAdapter.CarData data = (CarAdapter.CarData)parent.getItemAtPosition(position);
-                intent.putExtra("id", String.valueOf(data.ID));
-                startActivity(intent);
+            public void onResponse(String res) {
+                Log.d("FirstPage", "Response: " + res);
+                try {
+                    ListView lista = (ListView)findViewById(R.id.listView);
+                    JSONObject carsDescription = new JSONObject(res);
+                    List<CarAdapter.CarData> cars = new ArrayList<CarAdapter.CarData>();
+                    try {
+                        JSONArray results = carsDescription.getJSONArray("results");
+
+                        for(int i = 0; i < results.length(); i++) {
+                            JSONObject obj = results.getJSONObject(i);
+
+                            CarAdapter.CarData data = new CarAdapter.CarData(obj);
+                            cars.add(data);
+                        }
+                    }
+                    catch(JSONException e) {
+                        Log.d("FirstPage", "Could not parse description due to " + e.getMessage());
+                    }
+                    ArrayAdapter adapter = new CarAdapter(FirstPage.this, R.layout.elementy_listy_main, cars);
+
+                    lista.setAdapter(adapter);
+
+                    lista.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+                        @Override
+                        public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                            Intent intent = new Intent(FirstPage.this, RentACar.class);
+                            CarAdapter.CarData data = (CarAdapter.CarData) parent.getItemAtPosition(position);
+                            intent.putExtra("id", String.valueOf(data.ID));
+                            startActivity(intent);
+                        }
+                    });
+                }
+                catch(JSONException e) {
+                    Log.d("FirstPage", "Could not parse JSON: " + e.getMessage());
+                }
             }
-        });
+        }, new ApiController.GenericErrorListener("Category"));
+
 
     }
 
@@ -119,12 +133,15 @@ public class FirstPage extends ActionBarActivity {
 
     public void filterClick(View view){
         Intent intent = new Intent(FirstPage.this, FilterByPreference.class);
-        startActivity(intent);
+        startActivityForResult(intent, FilterByPreference.FILTER_RESULTS);
 
     }
 
     public void addressClick(View view) {
         Intent intent = new Intent(this, SelectAddress.class);
+        if(getIntent().hasExtra("filters"))
+            intent.putExtra("filters", getIntent().getSerializableExtra("filters"));
+        
         startActivity(intent);
     }
 
